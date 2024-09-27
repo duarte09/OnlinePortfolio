b66c7da855f66dfc33ee0c79affd44d2425b4ec0
Felipe <felipe.duarte@autoscar.com.br>
2024-09-27 18:19:00 -0300

    adicionando textos e ajustando foto

--- src/components/Sobre.jsx
+++ src/components/Sobre.jsx
@@ -9,18 +9,27 @@ const Sobre = () => {
           <div className="my-auto mx-6">
             <h2 className="text-4xl font-bold mb-4 primary-color">Sobre mim</h2>
             <p className="text-base lg:text-lg">
-              Yes, that's correct. I need to cancel checks from 10,001 to
-              90,000. Point for the Einsteins. [applause] Here's your final
-              question. Cinephiles, put on your memory berets: This 2001
-              masterpiece from Gilles Paquet-Brenner explores the intricate
-              dynamics of a family in disarray. Your screen is all black. You
-              just unplugged your computer. Uh, yes you are. I'm Assistant
-              Regional Manager. Now this is called slack lining. Oh.
+              Prazer! Me chamo Felipe, e hoje atuo como desenvolvedor de
+              software na empresa Autoscar, e estou graduando em Sistemas de
+              Informação na Universidade Federal de Uberlândia. 
+              <br />
+              <br />
+              Atualmente atuo utilizando Javascript, Nest.js, React e Node. E anteriormente,
+              adquiri experiência como analista de suporte e testes na iCrop
+              Gestão de Irrigação e como analista de suporte em redes na Algar
+              Telecom. Estou sempre buscando novos desafios e oportunidades para
+              aprender e crescer. 
+              <br />
+              <br />
+              Dentre minhas características profissionais
+              destacam-se a evolução contínua, capacidade analítica,
+              proatividade, orientação e capacitação de novos colaboradores,
+              saber lidar com situações criticas e de intervenção imediata.
             </p>
           </div>
         </div>
         <img
-          className="mx-auto rounded-3x1 py-8 md:py-0"
+          className="ml-auto rounded-3x1 py-8 md:py-0"
           src={sobre}
           width={300}
           height={300}
